3af290ecaa2ceee0b9716ff69353573ade26c6a4
Annie Schino <annaeva.kulakova@gmail.com>
2026-01-12 23:18:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/prediction-2026/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
