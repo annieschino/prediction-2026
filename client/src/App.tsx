7e91a87783d60a6cd93c2f058b28b79b7fa2cfc1
Annie Schino <annaeva.kulakova@gmail.com>
2026-01-13 00:06:11 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -22,7 +22,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <AudioPlayer />
-      <Router />
+
+      <WouterRouter base={import.meta.env.BASE_URL}>
+        <Router />
+      </WouterRouter>
+
     </QueryClientProvider>
   );
 }
